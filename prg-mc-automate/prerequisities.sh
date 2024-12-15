5a8c4cf8a4069c7b4cad15553a70c6f85c4e9dcc
luca-mocini <lucamocini12@gmail.com>
2024-12-15 22:09:30 +0100

    Add files via upload

---
+++ prg-mc-automate/prerequisities.sh
@@ -0,0 +1,7 @@
+# Installazione dei pacchetti di sistema
+sudo apt update -y
+sudo apt install python3 -y
+sudo apt install python3-pip xdotool wmctrl -y
+
+# Installazione dei pacchetti Python
+pip install pygetwindow pyautogui python-xlib
